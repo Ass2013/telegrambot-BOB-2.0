f0187609965fe7f2139de64edb5dc447ca24bf5f
Anton <ant.ser.sol@gmail.com>
2025-04-05 15:31:18 +0200

    Update bot main.py

--- bot main.py
+++ bot main.py
@@ -1,30 +1,27 @@
 import telebot
-from bot_logic import gen_pass, gen_emodji, flip_coin 
-from telebot import TeleBot
+from bot_logic import gen_pass, gen_emodji, flip_coin
 from telebot.handler_backends import ContinueHandling
 import time, threading, schedule
-from telebot import TeleBot
 
-bot = telebot.TeleBot("7703178680:AAGntMlCv91haAF8c8uKOye6p1uyImxhYcs")
+# Use an environment variable or a configuration file to store the token
+import os
+bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"))
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin  ")
-
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin")
 
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
     bot.reply_to(message, "Привет! Как дела?")
 
-
 @bot.message_handler(commands=['bye'])
 def send_bye(message):
     bot.reply_to(message, "Пока! Удачи!")
 
-
 @bot.message_handler(commands=['pass'])
 def send_password(message):
-    password = gen_pass(10)  
+    password = gen_pass(10)
     bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
 
 @bot.message_handler(commands=['emodji'])
@@ -37,10 +34,9 @@ def send_coin(message):
     coin = flip_coin()
     bot.reply_to(message, f"Монетка выпала так: {coin}")
     return ContinueHandling()
+
 if __name__ == '__main__':
     threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-bot.polling()
